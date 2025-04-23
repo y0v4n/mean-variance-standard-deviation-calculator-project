c7d7f0f82ebd03ce84bd56ce9ca3b8615d58ae2b
Jovan J. <j.j0v4n@pm.me>
2025-04-23 12:43:05 +0000

    Submitting my solution for the Mean-Variance-Standard Deviation Calculator Project.

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,17 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
+    if len(list) < 9:
+        raise ValueError("List must contain nine numbers.")
+    else:
+        matrix = np.reshape(list, (3,3))
+    
+    calculations = {
+                    'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.flatten().mean()],
+                    'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.flatten().var()],
+                    'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.flatten().std()],
+                    'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.flatten().max()],
+                    'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.flatten().min()],
+                    'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.flatten().sum()]
+    }
     return calculations
